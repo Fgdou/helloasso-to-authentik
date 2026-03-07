425718107839b946d0d1014166d9a49f5ba3cb3d
Fgdou <fabigoardou@gmail.com>
2026-03-07 11:34:14 +0100

    feat: payments retreival

--- src/helloasso/client.rs
+++ src/helloasso/client.rs
@@ -1,7 +1,8 @@
 use chrono::{DateTime, TimeDelta, Utc};
 
-use crate::helloasso::api::ClientAPI;
+use crate::helloasso::api::{ClientAPI, Form, Payment};
 
+#[derive(Debug)]
 pub struct HelloAsso {
     client_api: ClientAPI,
     client_id: String,
@@ -9,10 +10,12 @@ pub struct HelloAsso {
     refresh_token: String,
     expiration: DateTime<Utc>,
     organisation_slug: String,
+    forms: Vec<Form>,
+    payments: Vec<Payment>,
 }
 
 impl HelloAsso {
-    pub async fn new(client_id: String, client_secret: String) -> Self {
+    pub async fn new(client_id: String, client_secret: String, organisation_slug: String) -> Self {
         let client_api = ClientAPI::new("https://api.helloasso.com").unwrap();
 
         let token_response = client_api
@@ -22,13 +25,19 @@ impl HelloAsso {
 
         let expiration = Utc::now();
 
-        Self {
+        let mut res = Self {
             client_api,
             client_id,
             access_token: token_response.access_token,
             refresh_token: token_response.refresh_token,
             expiration,
-        }
+            organisation_slug,
+            forms: Default::default(),
+            payments: Default::default(),
+        };
+        //        res.fetch_forms().await;
+        res.fetch_payments().await;
+        res
     }
 
     async fn refresh_token(&mut self) {
@@ -49,5 +58,25 @@ impl HelloAsso {
         if self.is_token_expired().await {
             self.refresh_token().await
         }
+    }
+
+    async fn fetch_forms(&mut self) {
+        let orgs = self
+            .client_api
+            .get_organization_forms(self.organisation_slug.clone(), &self.access_token)
+            .await
+            .unwrap();
+
+        self.forms = orgs.data.unwrap_or(Vec::new());
+    }
+
+    async fn fetch_payments(&mut self) {
+        let payments = self
+            .client_api
+            .get_payments(self.organisation_slug.clone(), &self.access_token)
+            .await
+            .unwrap();
+
+        self.payments = payments.data.unwrap_or(Vec::new());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+use crate::helloasso::HelloAsso;
+
 mod helloasso;
 
 #[tokio::main]
 async fn main() {
     let client_id = "7cd522dd834e49b5bc26c6efd0cf4293";
     let client_secret = "y8QjUzceThp2jrriEs0rsutFB2tfITML";
+    let organization_slug = "comite-d-interets-de-quartier-du-panier-c-i-q-panier";
 
-    let client = helloasso::api::ClientAPI::new("https://api.helloasso.com").unwrap();
-    let token = client
-        .request_token(client_id.into(), client_secret.into())
-        .await
-        .unwrap();
+    let client = HelloAsso::new(
+        client_id.into(),
+        client_secret.into(),
+        organization_slug.into(),
+    )
+    .await;
 
-    println!("{token:?}");
+    dbg!(client);
 }
